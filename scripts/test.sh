352000e66b5ef12133d00256eb4370c0984aeaa7
DebajyotiX <debajyotimaths@gmail.com>
2024-10-13 11:29:17 +0530

    feat: add build and test scripts

---
+++ scripts/test.sh
@@ -0,0 +1,6 @@
+cmake -S test -B build/test
+cmake --build build/test
+CTEST_OUTPUT_ON_FAILURE=1 cmake --build build/test --target test
+
+# or simply call the executable: 
+./build/test/HTMLserverTests
